7dd7df030230e517ff5547188e2676b8fc318fb5
Nikita-Parus <cheems.nz@gmail.com>
2024-09-28 00:58:54 +0300

    Добавьте файлы проекта.

---
+++ intro_lb1_3/intro_lb1_3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, ".1251");
+	int a;
+	cout << "Введiть а:" << endl;
+	cin >> a;
+
+	cout << "а початкове:" << endl;
+	cout << a << endl;
+	a++;
+	cout << "а пiсля постфiксної iнкрементацiї:" << endl;
+	cout << a << endl;
+	++a;
+	cout << "а пiсля префiксної iнкрементацiї:" << endl;
+	cout << a << endl;
+	a--;
+	cout << "а пiсля постфiксної декрементацiї:" << endl;
+	cout << a << endl;
+	--a;
+	cout << "а пiсля префiксної декрементацiї:" << endl;
+	cout << a << endl;
+}
